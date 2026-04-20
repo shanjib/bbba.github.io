5e583a7090b62745cc8065054e4d3aa71aefb520
shanjib <ishanjib@gmail.com>
2026-04-20 14:54:42 -0400

    update teams

--- js/data/season-2026.js
+++ js/data/season-2026.js
@@ -15,10 +15,10 @@ var seasons = {
   ...( typeof seasons !== "undefined" ? seasons : {} ),
   "2026": {
     teams: [
-        {id: "team-a", name: "Team A", playerIds: []},
-        {id: "team-b", name: "Team B", playerIds: []},
-        {id: "team-c", name: "Team C", playerIds: []},
-        {id: "team-d", name: "Team D", playerIds: []},
+        {id: "team-a", name: "The Cuties", playerIds: ["p2", "p12", "p7", "p21", "p4", "p17"]},
+        {id: "team-b", name: "Team B", playerIds: ["p8", "p1", "p13", "p19", "p16"]},
+        {id: "team-c", name: "Team C", playerIds: ["p3", "p18", "p20", "p11", "p10"]},
+        {id: "team-d", name: "Team D", playerIds: ["p6", "p14", "p9", "p15", "p5"]},
     ],
     games: [
       // --- Matchup cycle 1 ---
